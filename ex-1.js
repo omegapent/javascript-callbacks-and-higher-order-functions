d939dca4379e21ac5d53c530d02c81b2a3dbeb67
Beyond <bbinthebloom@hotmail.com.com>
2024-11-09 04:19:49 +0700

    answered

--- ex-1.js
+++ ex-1.js
@@ -2,11 +2,19 @@
 
 function forEach(array, operation) {
   // Start coding here
+  for (let i=0; i < array.length; i++){
+    newEmployeeSalaries.push(operation(array[i]));
+  }
+    return newEmployeeSalaries; 
 }
+
+function add5000(array) {
+  return array + 5000;
+};
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
-// Using `forEach` function here
-
+// Using `forEach` function herez
+forEach(employeeSalaries, add5000)
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
